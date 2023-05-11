09f5fbbb9006df8d5e5e60b987390193c29c4c56
Michael Ripperger <michael.ripperger@swri.org>
2023-05-11 16:58:45 -0500

    Exposed plugins in separate library

--- src/display/ros_display.cpp
+++ src/display/ros_display.cpp
@@ -156,5 +156,3 @@ reach::Display::ConstPtr ROSDisplayFactory::create(const YAML::Node& config) con
 
 }  // namespace display
 }  // namespace reach_ros
-
-EXPORT_DISPLAY_PLUGIN(reach_ros::display::ROSDisplayFactory, ROSDisplay)
